b385b036f9ae0867108644a0bd80e287655d81c2
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-09-22 21:18:32 +0300

    create update blog route

--- app/main/views.py
+++ app/main/views.py
@@ -10,7 +10,7 @@ from ..email import mail_message
 @main.route('/')
 def index():
     quotes = get_quotes()
-    blogs = Blog.query.order_by(Blog.posted.desc()).limit(3).all()
+    blogs = Blog.query.order_by(Blog.posted.desc()).limit(10).all()
     return render_template('index.html', quote = quotes,blogs=blogs)
 
 @main.route('/profile/<name>')
@@ -52,6 +52,31 @@ def new_blog():
         return redirect(url_for('main.index'))
         flash('You Posted a new Blog')
     return render_template('newblog.html', form = form)
+@main.route('/blog/<id>')
+def blog(id):
+    blog = Blog.query.get(id)
+    return render_template('blog.html',blog=blog)
+    
+
+@main.route('/blog/<blog_id>/update', methods = ['GET','POST'])
+@login_required
+def updateblog(blog_id):
+    blog = Blog.query.get(blog_id)
+    if blog.user != current_user:
+        abort(403)
+    form = CreateBlog()
+    if form.validate_on_submit():
+        blog.title = form.title.data
+        blog.content = form.content.data
+        db.session.commit()
+        flash("You have updated your Blog!")
+        return redirect(url_for('main.blog',id = blog.id)) 
+    if request.method == 'GET':
+        form.title.data = blog.title
+        form.content.data = blog.content
+    return render_template('newblog.html', form = form)
+
+
 
 @main.route('/comment/<blog_id>', methods = ['Post','GET'])
 @login_required
