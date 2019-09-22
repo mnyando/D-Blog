e72853f92c889e68062a5005f8b3f4183c1cdb1c
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-09-22 11:08:07 +0300

    create update profile and progile methods

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,10 @@
-from flask import render_template,redirect,url_for
+from flask import render_template,redirect,url_for,abort
 from app.main import main
+from app.models import User
+from .forms import UpdateProfile
+from .. import db
 from app.requests import get_quotes
+from flask_login import login_required
 
 @main.route('/')
 def index():
@@ -8,6 +12,24 @@ def index():
     print(quotes)
     return render_template('index.html', quote = quotes)
 
-@main.route('/profile')
-def profile():
-    return render_template('profile/profile.html')
+@main.route('/profile/<name>')
+@login_required
+def profile(name):
+    user = User.query.filter_by(username = name).first()
+    if user == None:
+        abort(404)
+   
+    return render_template('profile/profile.html',user = user)
+
+@main.route('/user/<name>/updateprofile', methods = ['POST','GET'])
+@login_required
+def updateprofile(name):
+    form = UpdateProfile()
+    user = User.query.filter_by(username = name).first()
+    if user == None:
+        abort(404)
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+        user.save()
+        return redirect(url_for('.profile',name = name))
+    return render_template('profile/updateprofile.html',form =form)
