83dcb74ef19b7aab45838c640800a6a73215b7d2
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-09-22 16:06:37 +0300

    create subscribers model

--- app/models.py
+++ app/models.py
@@ -103,3 +103,16 @@ class Comment(db.Model):
 
     def __repr__(self):
         return f'Comment {self.comment}'
+
+class Subscriber(db.Model):
+    __tablename__='subscribers'
+
+    id=db.Column(db.Integer,primary_key=True)
+    email = db.Column(db.String(255),unique=True,index=True)
+
+    def save_subscriber(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'Subscriber {self.email}'
