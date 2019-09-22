214a5ffebbbdba29238384226bba6d6771854e8e
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-09-22 09:49:47 +0300

    query quotes using quotes api

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,12 @@
 from flask import render_template,redirect,url_for
 from app.main import main
+from app.requests import get_quotes
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    quotes = get_quotes()
+    print(quotes)
+    return render_template('index.html', quote = quotes)
 
 @main.route('/profile')
 def profile():
