6761783d46cda6c19a7d1bd308a52be316062b13
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-09-22 16:37:00 +0300

    Add Email message for a new subscriber

--- app/main/views.py
+++ app/main/views.py
@@ -62,7 +62,7 @@ def subscribe():
     email = request.form.get('subscriber')
     new_subscriber = Subscriber(email = email)
     new_subscriber.save_subscriber()
-    mail_message("Subscribed to D-Blog","email/welcome",new_subscriber.email,new_subscriber=new_subscriber)
+    mail_message("Subscribed to D-Blog","email/welcome_subscriber",new_subscriber.email,new_subscriber=new_subscriber)
     return redirect(url_for('main.index'))
 
 
