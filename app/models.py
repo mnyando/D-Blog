1a82b97f5a5c7074b026b6bac3678eaefc697317
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-09-22 11:08:56 +0300

    create form to enable a user to update thrie bio

--- app/models.py
+++ app/models.py
@@ -11,6 +11,8 @@ class User (UserMixin,db.Model):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255),unique = True,nullable = False)
     email = db.Column(db.String(255), unique = True,nullable = False)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
     hashed_password = db.Column(db.String(255),nullable = False)
     
     @property
