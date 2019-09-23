da16143a72ce690da7b4d3efa70943cfccd5c50a
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-09-23 13:05:16 +0300

    new method to save user profile into the database

--- app/main/views.py
+++ app/main/views.py
@@ -6,6 +6,8 @@ from .. import db
 from app.requests import get_quotes
 from flask_login import login_required,current_user
 from ..email import mail_message
+import secrets
+import os
 
 @main.route('/')
 def index():
@@ -13,16 +15,35 @@ def index():
     page = request.args.get('page',1, type = int )
     blogs = Blog.query.order_by(Blog.posted.desc()).paginate(page = page, per_page = 4)
     return render_template('index.html', quote = quotes,blogs=blogs)
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_filename = random_hex + f_ext
+    picture_path = os.path.join('app/static/photos', picture_filename)
+    form_picture.save(picture_path)
 
-@main.route('/profile/<name>')
+    return picture_filename
+
+@main.route('/profile',methods = ['POST','GET'])
 @login_required
-def profile(name):
-    user = User.query.filter_by(username = name).first()
-    profile_pic_path = url_for('static',filename = 'photos/'+ current_user.profile_pic_path)
-    if user == None:
-        abort(404)
-   
-    return render_template('profile/profile.html',user = user, profile_pic_path=profile_pic_path)
+def profile():
+    form = UpdateProfile()
+    if form.validate_on_submit():
+        if form.profile_picture.data:
+            picture_file = save_picture(form.profile_picture.data)
+            current_user.profile_pic_path = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        current_user.bio = form.bio.data
+        db.session.commit()
+        flash('Succesfully updated your profile')
+        return redirect(url_for('main.profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+        form.bio.data = current_user.bio
+    profile_pic_path = url_for('static',filename = 'photos/'+ current_user.profile_pic_path)   
+    return render_template('profile/profile.html', profile_pic_path=profile_pic_path, form = form )
 
 @main.route('/user/<name>/updateprofile', methods = ['POST','GET'])
 @login_required
